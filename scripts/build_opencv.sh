2efc1797effc4271651cd32af4e337f691795f78
AppSolves <appsolves@gmail.com>
2025-05-15 19:40:32 +0200

    Update build_opencv.sh: add error handling and success messages for OpenCV image build and push

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -13,4 +13,18 @@ cd "$(dirname "$0")/../opencv" || exit 1
 docker buildx build --platform linux/arm64 --build-context root=../ -f Dockerfile.opencv \
     -t ghcr.io/appsolves/lanepilot/opencv_base:latest .
 
-echo "✅ Done. OpenCV base image built successfully."
+if [ $? -ne 0 ]; then
+  echo "❌ Error: Failed to build the OpenCV base image."
+  exit 1
+else
+  echo "✅ OpenCV base image built successfully."
+  echo "🚀 Pushing the OpenCV base image to ghcr.io/appsolves/lanepilot/opencv_base:latest"
+  
+  docker push --platform linux/arm64 ghcr.io/appsolves/lanepilot/opencv_base:latest
+  if [ $? -ne 0 ]; then
+    echo "❌ Error: Failed to push the OpenCV base image."
+    exit 1
+  else
+    echo "✅ OpenCV base image pushed successfully."
+  fi
+fi
