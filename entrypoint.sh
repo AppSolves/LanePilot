9f939c13f54dfa80682e6c0cef704bcda8689fd0
AppSolves <appsolves@gmail.com>
2025-05-17 20:54:48 +0200

    Improve network interface retrieval in entrypoint script: handle multiple interfaces and add error checking

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 
-# Retrieve the network interface's name
-DEVICE_NAME=$(ls /sys/class/net | grep ^e)
+# Retrieve the ethernet network interface's name(s)
+ETH_INTERFACES=($(ls /sys/class/net | grep ^e))
+
+if [ ${#ETH_INTERFACES[@]} -eq 0 ]; then
+    echo "[ENTRYPOINT] Error: No ethernet interfaces found." >&2
+    exit 1
+fi
+
+DEVICE_NAME=${ETH_INTERFACES[0]}
 
 # Set static IP addresses for the containers
 ip addr add ${DEVICE_STATIC_IP}/24 dev ${DEVICE_NAME}
