6a72a7a8cbcf110d97ac6ad9c587e3dd173d4763
AppSolves <appsolves@gmail.com>
2025-05-17 20:41:51 +0200

    Refactor network interface handling in entrypoint script: use dynamic device name instead of hardcoded eth0

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,11 @@
 #!/bin/bash
 
+# Retrieve the network interface's name
+DEVICE_NAME=$(ls /sys/class/net | grep ^e)
+
 # Set static IP addresses for the containers
-ip addr add ${DEVICE_STATIC_IP}/24 dev eth0
-ip link set eth0 up
+ip addr add ${DEVICE_STATIC_IP}/24 dev ${DEVICE_NAME}
+ip link set ${DEVICE_NAME} up
 
 # Start the Python main application
 exec python3 -m src.main
