a8844d40065ef810c02de48486743e26c5c3bff7
AppSolves <appsolves@gmail.com>
2025-05-17 13:07:50 +0200

    Add --pull flag to Docker build commands for Raspberry Pi and OpenCV images

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -10,7 +10,7 @@ fi
 
 cd "$(dirname "$0")/../opencv" || exit 1
 
-docker buildx build --platform linux/arm64 --build-context root=../ -f Dockerfile.opencv \
+docker buildx build --pull --platform linux/arm64 --build-context root=../ -f Dockerfile.opencv \
     -t ghcr.io/appsolves/lanepilot/opencv_base:latest .
 
 if [ $? -ne 0 ]; then
