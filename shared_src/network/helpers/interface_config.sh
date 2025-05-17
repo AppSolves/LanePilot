1b64469f57e7fa11f58d7278c510c0272c597664
AppSolves <appsolves@gmail.com>
2025-05-17 22:23:06 +0200

    Fix log message prefix in interface configuration script for clarity

--- shared_src/network/helpers/interface_config.sh
+++ shared_src/network/helpers/interface_config.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# Set the default IP address for the ethernet interface
+if [ -z "$DEVICE_STATIC_IP" ]; then
+    DEVICE_STATIC_IP="0.0.0.0"
+    echo "[INTERFACE CONFIG] Warning: DEVICE_STATIC_IP is not set. Defaulting to 0.0.0.0."
+fi
+
 # Retrieve the ethernet network interface's name(s)
 ETH_INTERFACES=($(ls /sys/class/net | grep ^e))
 
 if [ ${#ETH_INTERFACES[@]} -eq 0 ]; then
-    echo "[ENTRYPOINT] Error: No ethernet interfaces found." >&2
+    echo "[INTERFACE CONFIG] Error: No ethernet interfaces found." >&2
     exit 1
 fi
 
