748b91c11ac0ceb32a506db5aada2b407764cd64
AppSolves <appsolves@gmail.com>
2025-05-19 12:18:11 +0200

    Enhance build_opencv.sh to include Jetson image build prompt and refine push image confirmation

--- scripts/build_opencv.sh
+++ scripts/build_opencv.sh
@@ -19,17 +19,32 @@ if [ $? -ne 0 ]; then
 else
   echo "✅ OpenCV base image built successfully."
 
+  # Check if the video_codec_sdk folder is not empty
+  if [ -d "video_codec_sdk" ] && [ "$(ls -A video_codec_sdk)" ]; then
+    # Ask the user if they want to build the jetson image too
+    read -p "Do you want to build the Jetson image as well? (y/n): " build_jetson
+    if [[ ! "$build_jetson" =~ ^[Yy]$ ]]; then
+      echo "🚫 Skipping Jetson image build."
+    else
+      echo "🚀 Building the Jetson image..."
+      docker buildx build --pull --platform linux/arm64 --build-context root=../ --build-context models=../../assets/trained_models \
+          -f Dockerfile.jetson \
+          -t ghcr.io/appsolves/lanepilot/jetson:latest .
+      if [ $? -ne 0 ]; then
+        echo "❌ Error: Failed to build the Jetson image."
+        exit 1
+      else
+        echo "✅ Jetson image built successfully."
+        exit 0
+      fi
+    fi
+  fi
+
   # Ask the user if they want to push the image
-  read -p "Do you want to push the OpenCV base image to ghcr.io/appsolves/lanepilot? (y/n): " push_image
+  read -p "Do you want to push the OpenCV base image to ghcr.io/appsolves/lanepilot/opencv_base:latest? (y/n): " push_image
   if [[ ! "$push_image" =~ ^[Yy]$ ]]; then
     echo "🚫 Skipping image push."
     exit 0
-  fi
-  
-  # Check if the video_codec_sdk folder is not empty
-  if [ -d "video_codec_sdk" ] && [ "$(ls -A video_codec_sdk)" ]; then
-    echo "❌ Error: video_codec_sdk folder is not empty. Pushing the image would violate NVIDIA's EULA."
-    exit 1
   fi
 
   echo "🚀 Pushing the OpenCV base image to ghcr.io/appsolves/lanepilot/opencv_base:latest"
