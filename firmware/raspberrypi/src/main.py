01a4fea179769465c1ae8640f28cd1f859f3a10c
AppSolves <appsolves@gmail.com>
2025-05-20 16:39:15 +0200

    Refactor signal handling to stop multiple threads gracefully on termination

--- firmware/raspberrypi/src/main.py
+++ firmware/raspberrypi/src/main.py
@@ -1,10 +1,19 @@
 import signal
 
-from shared_src.common import run_with_retry
+from shared_src.common import StoppableThread, run_with_retry
 from shared_src.network import NETWORK_CONFIG, ServerClient, discover_peer
 
 from .hardware_control import MODULE_CONFIG, ServoManager
 from .network import GStreamerSender, logger
+
+
+def stop_threads(threads: list[StoppableThread]) -> None:
+    """
+    Stops all threads in the provided list.
+    """
+    for thread in threads:
+        thread.stop()
+        thread.join()
 
 
 def start_network(tcp_port: int, udp_port: int) -> None:
@@ -25,7 +34,9 @@ def start_network(tcp_port: int, udp_port: int) -> None:
     servo_manager.try_reinit()
     server_thread.add_listener(servo_manager.on_event)
 
-    signal.signal(signal.SIGTERM, lambda _, __: server_thread.stop())
+    signal.signal(
+        signal.SIGTERM, lambda _, __: stop_threads([server_thread, gstreamer_thread])
+    )
     gstreamer_thread.join()
     server_thread.stop()
     servo_manager.dispose()
