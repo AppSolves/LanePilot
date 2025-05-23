8d64332d67a45292a427460094c399533f5a78bb
AppSolves <appsolves@gmail.com>
2025-05-23 09:57:57 +0200

    Refactor logging in model export functions to use info level and update return type for export_model_to_trt

--- shared_src/postprocessing/model_export.py
+++ shared_src/postprocessing/model_export.py
@@ -1,4 +1,3 @@
-import os
 import warnings
 
 from shared_src.common import IS_DEBUG
@@ -40,7 +39,7 @@ def export_model_to_onnx(
     model = model.eval().to(DEVICE)
 
     # Export the model to ONNX format
-    logger.debug("Exporting the model to ONNX format...")
+    logger.info("Exporting the model to ONNX format...")
     torch.onnx.export(
         model,
         dummy_input,
@@ -55,7 +54,7 @@ def export_model_to_onnx(
         dynamic_axes=dynamic_axes,
     )
 
-    logger.debug(f"ONNX model exported to '{save_path}'!")
+    logger.info(f"ONNX model exported to '{save_path}'!")
     return save_path
 
 
@@ -66,8 +65,8 @@ def export_model_to_trt(
     input_names: Optional[list[str]] = None,
     output_names: Optional[list[str]] = None,
     dynamic_axes: Optional[dict[str, dict[int, str]]] = None,
-    shapes: Optional[dict[str, str]] = None,
-) -> None:
+    shapes: Optional[dict[str, str | None]] = None,
+) -> Path:
     """
     Export a PyTorch model to TensorRT format.
     Args:
@@ -79,15 +78,23 @@ def export_model_to_trt(
         dynamic_axes (dict[str, dict[int, str]], optional): Dynamic axes for the model.
     """
     if isinstance(model, YOLO):
-        model.export(
+        logger.info("Exporting YOLO model to TensorRT format...")
+        path = model.export(
             format="engine",
             dynamic=True,
             simplify=True,
             device="cuda",
         )
+        return Path(path)
     else:
         if save_path is None:
             raise ValueError("Save path is required for exporting the model.")
+        if shapes is None:
+            shapes = {
+                "min_shapes": None,
+                "opt_shapes": None,
+                "max_shapes": None,
+            }
         if dummy_input is None:
             raise ValueError("Dummy input is required for exporting the model.")
 
@@ -101,11 +108,12 @@ def export_model_to_trt(
             dynamic_axes=dynamic_axes,
         )
 
+        logger.info("Exporting the model to TensorRT format...")
         onnx_path = save_path.with_suffix(".onnx")
         trt_path = save_path.with_suffix(".engine")
 
         # Check if ONNX file exists before running trtexec
-        if not os.path.exists(onnx_path):
+        if not onnx_path.is_file():
             raise FileNotFoundError(f"ONNX file not found: {onnx_path}")
 
         # Properly quote the paths for the trtexec command
@@ -117,17 +125,17 @@ def export_model_to_trt(
             "--skipInference",
             "--versionCompatible",
             (
-                f"--minShapes={shapes.get("min_shapes")}"
+                f"--minShapes={shapes.get('min_shapes')}"
                 if shapes.get("min_shapes")
                 else ""
             ),
             (
-                f"--optShapes={shapes.get("opt_shapes")}"
+                f"--optShapes={shapes.get('opt_shapes')}"
                 if shapes.get("opt_shapes")
                 else ""
             ),
             (
-                f"--maxShapes={shapes.get("max_shapes")}"
+                f"--maxShapes={shapes.get('max_shapes')}"
                 if shapes.get("max_shapes")
                 else ""
             ),
@@ -146,4 +154,5 @@ def export_model_to_trt(
             raise RuntimeError(f"trtexec failed with error: {ret.stderr}")
 
         logger.debug(ret.stdout)
-        logger.debug(f"TensorRT model exported to '{trt_path}'!")
+        logger.info(f"TensorRT model exported to '{trt_path}'!")
+        return trt_path
